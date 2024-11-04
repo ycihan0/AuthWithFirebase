f2fcbcde4e2a5dc051df19d7a87286186db56cb8
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-11-04 16:05:22 +0300

    LoginScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
 
 export default function LoginScreen() {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="Email" />
+        <TextInput style={styles.input} placeholder="Şifre" />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Giriş Yap</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.outlineButton]}>
+          <Text style={styles.outlineButtonText}>Kayıt Ol</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
