c2eb49bf8576c582de4e30323329b39f7b46f51c
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-11-04 16:06:54 +0300

    LoginScreen CSS

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -27,4 +27,44 @@ export default function LoginScreen() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginTop: 5,
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    width: "60%",
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: "#0782F9",
+    padding: 15,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: 700,
+  },
+  outlineButton: {
+    backgroundColor: "white",
+    marginTop: 5,
+  },
+  outlineButtonText: {
+    color: "#0782F9",
+    fontSize: 16,
+    fontWeight: 700,
+  },
+});
