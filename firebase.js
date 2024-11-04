56b5b4fcbb23f3cee552c64295e42ae5bb8b6fd2
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-11-04 22:35:19 +0300

    login

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,7 +12,7 @@ const firebaseConfig = {
   projectId: "login-b7027",
   storageBucket: "login-b7027.firebasestorage.app",
   messagingSenderId: "519718668094",
-  appId: "1:519718668094:web:19797822960354a81a0e6f",
+  appId: "1:519718668094:web:19797822960354a81a0e6f"
 };
 
 // Initialize Firebase
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,11 +6,43 @@ import {
   TextInput,
   TouchableOpacity,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { auth } from "../firebase";
+import { useNavigation } from "@react-navigation/native";
 
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigation.navigate("Home");
+      }
+    });
+  }, []);
+
+  const handleSignUp = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log("kullanıcı", user.email);
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  const handleLogin = () => {
+    auth
+      .signInAnonymously(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log("kullanıcı giriş yaptı", user.email);
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.inputContainer}>
@@ -24,15 +56,18 @@ export default function LoginScreen() {
           style={styles.input}
           placeholder="Şifre"
           secureTextEntry
-          value={email}
+          value={password}
           onChangeText={(text) => setPassword(text)}
         />
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity onPress={handleLogin} style={styles.button}>
           <Text style={styles.buttonText}>Giriş Yap</Text>
-        </TouchableOpacity >
-        <TouchableOpacity onPress={handleSignUp} style={[styles.button, styles.outlineButton]}>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.outlineButton]}
+        >
           <Text style={styles.outlineButtonText}>Kayıt Ol</Text>
         </TouchableOpacity>
       </View>
@@ -69,7 +104,7 @@ const styles = StyleSheet.create({
   buttonText: {
     color: "white",
     fontSize: 16,
-    fontWeight: 700,
+    fontWeight: "700",
   },
   outlineButton: {
     backgroundColor: "white",
@@ -78,6 +113,6 @@ const styles = StyleSheet.create({
   outlineButtonText: {
     color: "#0782F9",
     fontSize: 16,
-    fontWeight: 700,
+    fontWeight: "700",
   },
 });
