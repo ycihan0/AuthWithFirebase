7173280283ac433de24fc69d66d3432523a5e547
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-11-04 22:50:53 +0300

    logout

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { auth } from "../firebase";
+import { useNavigation } from "@react-navigation/native";
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.navigate("Login");
+      })
+      .catch((error) => alert(error.message));
+  };
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <Text>Email: {auth.currentUser?.email}</Text>
+      <TouchableOpacity onPress={handleSignOut} style={styles.button}>
+        <Text style={styles.buttonText}>Çıkış Yap</Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#0782F9",
+    padding: 15,
+    alignItems: "center",
+    borderRadius: 10,
+    width: "60%",
+    marginTop: 50,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "700",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
